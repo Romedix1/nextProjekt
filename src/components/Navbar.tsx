59b7952050c3e38fbb9e68a8db1e09170f92bf68
pusiokamil <21297@student.ans-elblag.pl>
2026-02-07 19:04:31 +0100

    Ikonka

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { signOut } from "@/app/auth/actions";
 import { getUser } from "@/lib/getUser";
 import Link from "next/link";
-
+import Image from "next/image";
 export default async function Navbar() {
   const user = await getUser();
   const isAuthenticated = !!user;
@@ -12,7 +12,7 @@ export default async function Navbar() {
         <div className="flex justify-between items-center h-16">
           
           <Link href="/" className="text-2xl font-bold tracking-tight text-gray-900 hover:text-gray-700 transition">
-            PERFUMY
+            <Image src="/images/logo.png" alt="logo" width={150} height={50} />
           </Link>
             <div className="flex items-center space-x-4">
                 <Link href="/mezczyzni" className=" font-medium text-gray-700 hover:text-black transition">
@@ -35,7 +35,21 @@ export default async function Navbar() {
 
 
             <Link href="/cart" className="relative group text-gray-700 hover:text-black transition">
-              <span className="text-xl">Koszyk</span>
+              <span className="text-xl"><svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="24" 
+                height="24" 
+                viewBox="0 0 24 24" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="1.5" 
+                strokeLinecap="round" 
+                strokeLinejoin="round"
+                > 
+                <circle cx="9" cy="21" r="1"></circle>
+                <circle cx="20" cy="21" r="1"></circle>
+                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+              </svg></span>
             </Link>
           </div>
         </div>
